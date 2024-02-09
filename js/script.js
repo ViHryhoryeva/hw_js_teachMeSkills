f15db10926825a258f65668f96976dc286930488
ViHryhoryeva <hryhoryeva.viktoria@gmail.com>
2024-02-09 11:10:42 +0300

    task 2

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ let y = 58;
 let z = 42;
 let sum = x + y + z;
 console.log(sum);
+
+// задача 2
+let secOfMin = 60;
+let minOfHour = 60;
+let hoursOfFDay = 24;
+let dayOfYear = 365;
+let myAge = 28;
+let myAgeInSeconds = myAge * secOfMin * minOfHour * hoursOfFDay * dayOfYear;
+console.log(myAgeInSeconds);
