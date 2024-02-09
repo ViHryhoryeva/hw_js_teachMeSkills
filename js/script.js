631784ac5bb3ceef58d1d7cbb085edf35c0d11de
ViHryhoryeva <hryhoryeva.viktoria@gmail.com>
2024-02-09 11:15:20 +0300

    task 3

--- js/script.js
+++ js/script.js
@@ -13,3 +13,15 @@ let dayOfYear = 365;
 let myAge = 28;
 let myAgeInSeconds = myAge * secOfMin * minOfHour * hoursOfFDay * dayOfYear;
 console.log(myAgeInSeconds);
+
+// задача 3
+let count = 42;
+console.log('' + count);
+console.log(String(count));
+console.log(count.toString());
+console.log(`${count}`);
+
+let userName = '42';
+console.log(+userName);
+console.log(Number(userName));
+console.log(parseInt(userName));
