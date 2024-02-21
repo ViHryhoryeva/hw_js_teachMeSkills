38c31bd27a396a3322302210f6a3dc6f601aa5a2
ViHryhoryeva <hryhoryeva.viktoria@gmail.com>
2024-02-21 17:10:11 +0300

    task 4, task 5, task 6, task 7

--- js/script_hw_3.js
+++ js/script_hw_3.js
@@ -21,3 +21,55 @@ for (let i = 1; i <= 20; i++) {
         console.log(i);
     }
 }
+
+// залача 4
+let key = true;
+let documents = true;
+let pen = true;
+let apple = false;
+let orange = true;
+
+let shouldGoToWork = apple || orange && key && documents && pen;
+console.log(shouldGoToWork);
+
+// задача 5
+let i = prompt('Введите число');
+
+if (i % 5 == 0) {
+    console.log('Fiz');
+} else if (i % 3 == 0) {
+    console.log('Buz');
+} else if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizBuz');
+} else {
+    console.log('Число не делится на 3 и 5');
+}
+
+// задача 6
+let j = prompt('Введите ваш возраст:');
+if (j > 18) {
+    console.log('Попей пивка');
+} else if (j < 18) {
+    console.log('Пей колу');
+} else if (16 <= j <= 18) {
+    console.log('Можешь выкурить сигаретку, только маме не говори');
+}
+
+// задача 7
+let direction = prompt('В какую сторону света вы бы хотели б отправиться?');
+switch (direction) {
+    case 'юг': 
+    console.log('На юг пойдешь, счастье найдешь');
+    break;
+    case 'север': 
+    console.log('На север пойдешь, много денег найдушь');
+    break;
+    case 'запад':
+    console.log('На запад пойдешь, верного друга найдешь');
+    break;
+    case 'восток': 
+    console.log('На восток пойдешь, разработчиком станешь');
+    break;
+    default: 
+    prompt('Введите еще раз, в какую сторону света вы бы хотели б отправиться');
+}
