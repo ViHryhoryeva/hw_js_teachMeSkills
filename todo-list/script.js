40c252b74fa15b9cb1a5eae92347d4b0df82d66c
ViHryhoryeva <hryhoryeva.viktoria@gmail.com>
2024-03-25 17:28:36 +0300

    localstorage, tried to fix checkbox

--- todo-list/script.js
+++ todo-list/script.js
@@ -2,31 +2,33 @@
 const root = document.getElementById('root');
 
 const headerForm = document.createElement('form');
-    headerForm.classList.add('header');
-    root.appendChild(headerForm);
+headerForm.classList.add('header');
+root.appendChild(headerForm);
 
 const headerDelete = document.createElement('button');
-    headerDelete.classList.add('header__btn');
-    headerDelete.innerHTML = 'Delete ALL';
-    headerForm.appendChild(headerDelete);
+headerDelete.classList.add('header__btn');
+headerDelete.innerHTML = 'Delete ALL';
+headerForm.appendChild(headerDelete);
 
 const headerText = document.createElement('input');
-    headerText.setAttribute('type', 'text');
-    headerText.className = 'header__text';
-    headerText.setAttribute('placeholder', 'Enter todo ...');
-    headerForm.appendChild(headerText);
+headerText.setAttribute('type', 'text');
+headerText.className = 'header__text';
+headerText.setAttribute('placeholder', 'Enter todo ...');
+headerForm.appendChild(headerText);
 
 const headerAdd = document.createElement('button');
-    headerAdd.className = 'header__btn';
-    headerAdd.innerHTML = 'Add';
-    headerForm.appendChild(headerAdd);
+headerAdd.className = 'header__btn';
+headerAdd.innerHTML = 'Add';
+headerForm.appendChild(headerAdd);
 
 const mainUl = document.createElement('ul');
-    mainUl.classList.add('main');
-    root.appendChild(mainUl);
+mainUl.classList.add('main');
+root.appendChild(mainUl);
 
-let taskList = {};
 let count = 0;
+let taskList = getData();
+console.log(Object.keys(taskList).length);
+showTaskLisk();
 
 function Task(id, name) {
     this.id = id;
@@ -37,13 +39,15 @@ function Task(id, name) {
 
 function addTask(name) {
     count++;
-    let task = new Task(count, name);
+    let task = new Task(crypto.randomUUID(), name);
     taskList[task.id] = task;
+    setData(taskList);
     showTaskLisk();
 }
 
 function deleteTask(id) {
     delete taskList[id];
+    setData(taskList);
     showTaskLisk();
 }
 
@@ -53,6 +57,7 @@ function setDone(id, isDone) {
     } else {
         taskList[id].isDone = false;
     }
+    setData(taskList);
     showTaskLisk();
 }
 
@@ -60,18 +65,18 @@ function deleteAllTask() {
     for (let key in taskList) {
         delete taskList[key];
     }
+    setData(taskList);
     showTaskLisk();
 }
 
-function datetimeFormat(datetime) {
-    let year = datetime.getFullYear();
-    let month = datetime.getMonth();
-    let day = datetime.getDate();
-    let hour = datetime.getHours();
-    let min = datetime.getMinutes();
-    let sec = datetime.getSeconds();
-
-    return `${day}-${month}-${year} ${hour}:${min}:${sec}`;
+function datetimeFormat() {
+    const date = new Date();
+    const month = date.toLocaleString('en', {month: 'long'});
+    const day = date.getDate();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    
+    return `${hour}:${min} ${day}-${month}`;
 }
 
 function showTaskLisk() {
@@ -81,30 +86,37 @@ function showTaskLisk() {
     }
 
     for (let key in taskList) {
+        // console.log(taskList.length);
         // console.log(taskList[key]);
         const container = document.createElement('li');
         container.classList.add('container');
+        
         if (taskList[key].isDone === true) {
             container.style.backgroundColor = '#ce93d8';
         }
         mainUl.appendChild(container);
-        
+
+        const mainLabel = document.createElement('label');
         const mainCheckbox = document.createElement('input');
         mainCheckbox.setAttribute('type', 'checkbox');
         mainCheckbox.classList.add('main__yes');
-        mainCheckbox.setAttribute('id', 'check');
+        // mainCheckbox.setAttribute('id', 'check');
 
-        if (taskList[key].isDone) {
-            mainCheckbox.checked = true;
+        if (taskList[key].isDone === true) {
+            mainCheckbox.checked = true; 
+            mainLabel.style.before = '&check;';
+        } else {
+            
         }
-        container.appendChild(mainCheckbox);
-        const mainLabel = document.createElement('label');
-        mainLabel.setAttribute('for', 'check');
+ 
+        // mainLabel.setAttribute('for', 'check');
+        mainLabel.appendChild(mainCheckbox);
         container.appendChild(mainLabel);
 
         const mainText = document.createElement('p');
         mainText.classList.add('main__text');
         mainText.textContent = taskList[key].name;
+
         if (taskList[key].isDone === true) {
             mainText.style.textDecoration = 'line-through';
         }
@@ -149,3 +161,27 @@ headerDelete.addEventListener('click', (e) => {
 headerForm.addEventListener('submit', (e) => {
     e.preventDefault();
 })
+
+
+var todos = localStorage.getItem('todos');
+
+// var localStorageTodosKey = 'todos';
+
+function setData(value) {
+    if (value === undefined) {
+        return;
+    }
+    console.log(Object.keys(taskList).length);
+    localStorage.setItem('todos', JSON.stringify(value));
+}
+
+function getData() {
+    if (localStorage.getItem('todos') === null) {
+        let arr = {};
+        return arr;
+    } else {
+        return JSON.parse(localStorage.getItem('todos'))
+    }
+}
+
+
